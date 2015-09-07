22c5a6daafb39b5d283c429f55319bc6d85f9057
meith <mj9617@g.rit.edu>
2015-09-07 16:51:01 -0400

    Simple collisions working. Need to test with controllers and multiple players.

---
+++ Assets/Scripts/WarriorCollision.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarriorCollision : MonoBehaviour {
+
+	private bool collided;
+	private float hitDisplacement = 5000.0f;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		collided = false;
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		if(collided)
+		{
+			rb.AddForce(-transform.forward * hitDisplacement);
+			collided = false;
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Debug.Log("I got hit");
+			collided = true;
+		}
+	}
+}
